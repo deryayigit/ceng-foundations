0df70cd824ad5f62057927d5fdd34a1fd30ac790
Derya Yigit <48148717+deryayigit@users.noreply.github.com>
2025-08-09 11:11:42 +0300

    Update factorial_iterative.c

--- algorithms/basics/factorial_iterative.c
+++ algorithms/basics/factorial_iterative.c
@@ -1,16 +1,17 @@
+#include <stdio.h>
+
 int factorial(int n) {
-int result = 1; 
-for(int i =1; i <= n; i++) {
-result *= i;
-}
-return result;
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
 
-int main()
-{
-int num;
-printf("Enter a number: ");
-scanf("%d", &num);
-printf("Factorial of %d is %d\n", num, factorial(num));
-return 0;
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Factorial of %d is %d\n", num, factorial(num));
+    return 0;
 }
