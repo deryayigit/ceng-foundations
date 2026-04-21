08d9666fe7380df0e33c558d6a7e4753ddcd1159
Derya Yigit <deryayigidim@gmail.com>
2026-04-22 00:53:03 +0300

    Add Newton-Raphson method implementation in ASM

---
+++ computer-architecture/03_calc_integer_roots/newton_raphson.asm
@@ -0,0 +1,24 @@
+	ORG 0100H
+	MOV DH, 67H
+	MOV AL, 04H
+	MOV SI, 0130H
+	MOV CH, 00H
+LOOP:	
+	INC CH
+	MOV [SI], AL
+	MOV [SI+1], AL
+	ADD [SI+1], AL
+	MUL AL
+	ADD AL, DH
+	MOV BH, [SI+1]
+	DIV BH
+	CMP AL,[SI]
+	JZ FINISH
+ 	JMP LOOP
+
+FINISH:	MOV     BX,0000H  ;Returns to PAT System
+        MOV     AH,04H       
+        INT     028H
+                    
+
+                        
