cfc9b8cd26827b6e7c91c4ea80b17a7477a48b46
Derya Yigit <deryayigidim@gmail.com>
2026-04-22 00:47:45 +0300

    Add triangle wave generator in assembly

---
+++ computer-architecture/02_microbased_signal_generator/triangle_wave.asm
@@ -0,0 +1,40 @@
+#start=Grafik.exe#
+
+#make_bin#
+
+name "triangle"
+
+START:
+	MOV BL, 00H
+	MOV AL, 00H
+HIGH:
+    PUSH AX	
+    call waitport    
+    POP AX
+    OUT 160, AL
+    INC AL
+    CMP AL,0FFH
+	  JNZ HIGH
+	  MOV AL, 0FFH
+    
+LOW:  
+    PUSH AX	
+    call waitport    
+    POP AX
+    OUT 160, AL
+    DEC AL
+    CMP AL,0FFH
+
+	  JNZ LOW
+	
+	;JMP START
+
+
+hlt
+    
+waitport:
+    wait3:           ; loop to ensure the printer
+    in al, 160d     ; is ready, it clears
+    or al, 0        ; the port when this is true.
+    jnz wait3
+    ret
